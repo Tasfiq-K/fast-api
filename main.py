67424aea329cc5f02583bbd9af15cbef1e9b3b73
Tasfiq Kamran <52997915+Tasfiq-K@users.noreply.github.com>
2025-06-17 15:20:44 +0000

    --source python file and data added

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+import json
+
+app = FastAPI()
+
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+
+    return data
+
+@app.get("/")
+def hello():
+    return {'message': 'Patient management system API'}
+
+@app.get('/about')
+def about():
+    return {
+        'message': "A fully functional API to manage your patient records" 
+    }
+
+@app.get("/view")
+def view():
+    data = load_data()
+
+    return data
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    
+    return {'error': 'Patient Not Found'}
